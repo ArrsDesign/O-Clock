bd0606b6e65c396b6bd1017aa770bcce387bf270
ArrsDesign <Hack&2098>
2020-09-16 10:59:36 -0500

    calendar
    calendar made

--- app/src/main/java/com/example/oclock/Home.java
+++ app/src/main/java/com/example/oclock/Home.java
@@ -3,6 +3,7 @@ package com.example.oclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        CalendarView calendar = findViewById(R.id.calendar);
     }
 }
